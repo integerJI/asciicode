26dcff680adea80ad357d159d406ad97afcff027
integerJI <jjs9536@gmail.com>
2020-02-16 09:19:05 +0900

    2

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn asciiproject.wsgi --log-file -
--- asciiproject/settings.py
+++ asciiproject/settings.py
@@ -20,12 +20,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'eo7s)$*$$15fwf+h$-)*jj#k1i6%4&f*@7@e0nup4q-o3kvr=e'
-
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'eo7s)$*$$15fwf+h$-)*jj#k1i6%4&f*@7@e0nup4q-o3kvr=e')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -41,6 +40,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -119,3 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
